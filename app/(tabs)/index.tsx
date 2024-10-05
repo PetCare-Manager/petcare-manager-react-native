e8e75c603d0329183f8fbe5ff8d753b7e39ee3e1
DarelNC <twh.darelnc@hotmail.com>
2024-10-05 14:05:39 +0200

    Add tailwindcss

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,12 @@
-import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View className="flex-1 justify-center items-center !bg-teal-200">
+      <Text className="text-[20px] font-bold">Tab One</Text>
+      <View className="my-[30px] h-1 w-[80%]" lightColor="#eee" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
