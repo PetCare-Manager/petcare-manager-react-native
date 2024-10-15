197e5195fafdd0df80f77f988d4f1ea2c7bf6a5f
DarelNC <twh.darelnc@hotmail.com>
2024-10-15 21:41:20 +0200

    Fix smth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,22 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
 import "../assets/css/_tailwind.css";
-import { useColorScheme } from "@/components/useColorScheme";
+
+import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from "expo-router";
+} from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: '(tabs)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -26,7 +24,8 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    AfacadFluxRegular: require("../assets/fonts/AfacadFlux-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -51,10 +50,10 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
   );
--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,10 +1,5 @@
-import { Text, TextProps } from "./Themed";
+import { Text, TextProps } from './Themed';
 
 export function MonoText(props: TextProps) {
-  return (
-    <Text
-      {...props}
-      style={[props.style, { fontFamily: "AfacadFlux-Regular" }]}
-    />
-  );
+  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
 }
