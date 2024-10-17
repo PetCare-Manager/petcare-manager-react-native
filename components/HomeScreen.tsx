1192f1a7ad8bfcf770dd1a063b5d8dc2f7aa3f0b
Carol-88 <gralleta@gmail.com>
2024-10-17 17:15:17 +0200

    working in ui

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, View, Text, TextInput, TouchableOpacity } from "react-native";
+import { Image, View, Text, TouchableOpacity } from "react-native";
 import logo from "@/assets/images/logo.png";
 
 export const HomeScreen = () => {
@@ -7,34 +7,24 @@ export const HomeScreen = () => {
     <View className="flex-1 items-center justify-center bg-white">
       <Image source={logo} className="!w-[160px] !h-[220px] mb-8" />
       <View className="w-full px-6">
-        <Text className="text-3xl font-bold text-blue-500 mb-8">
+        <Text className="text-3xl font-bold text-center text-gray-800 mb-8">
           PetCare Manager
         </Text>
-        {/* Inputs para registro */}
-        <TextInput
-          placeholder="Correo electrónico"
-          keyboardType="email-address"
-          className="border border-gray-300 rounded-lg py-2 px-4 mb-4 w-full"
-        />
-        <TextInput
-          placeholder="Contraseña"
-          secureTextEntry={true}
-          className="border border-gray-300 rounded-lg py-2 px-4 mb-8 w-full"
-        />
-        {/* Botones de registro */}
-        <TouchableOpacity className="bg-blue-500 rounded-lg py-2 px-4 mb-4 w-full items-center justify-center">
-          <Text className="text-white">Registrar</Text>
-        </TouchableOpacity>
-        {/* Botones de redes sociales */}
-        <TouchableOpacity className="bg-red-500 rounded-lg py-2 px-4 mb-4 w-full items-center justify-center">
-          <Text className="text-white">Google</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="bg-purple-600 rounded-lg py-2 px-4 mb-4 w-full items-center justify-center">
-          <Text className="text-white">Instagram</Text>
-        </TouchableOpacity>
-        <TouchableOpacity className="bg-blue-400 rounded-lg py-2 px-4 w-full items-center justify-center">
-          <Text className="text-white">X</Text>
-        </TouchableOpacity>
+
+        <Text className="text-l text-center text-gray-800 mb-28">
+          Lleva todo lo que necesitas en el bolsillo
+        </Text>
+
+        {/* Contenedor para los botones en fila */}
+        <View className="flex-row justify-between mb-4">
+          <TouchableOpacity className="bg-white rounded-lg border border-indigo-500  py-2 px-4 flex-1 mr-2 items-center justify-center">
+            <Text className="text-indigo-500">Registro</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity className="bg-indigo-500 rounded-lg py-2 px-4 flex-1 ml-2 items-center justify-center">
+            <Text className="text-white">Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
