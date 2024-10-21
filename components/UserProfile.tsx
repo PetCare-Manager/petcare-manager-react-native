a1806ad4e36f87940d55f77d65a23e686ed84877
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-10-21 18:43:19 +0200

    Style the structure of each component

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export const UserProfile = () => {
     return (
-        <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
-            User Profile
-        </Text>
+        <View>
+            <View>
+                <Text className="font-afacad-semibold text-3xl text-center text-gray-800">
+                    Navbar. Menú: añadir mascota, añadir evento
+                </Text>
+            </View>
+            <View className="flex justify-center m-5 items-center" >
+                <FontAwesome6 name="person" size={50} color="black" />
+            </View>
+            <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
+                ¡Hola, -Name-! Disfruta tu día
+            </Text>
+            <View>
+                <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
+                    PetCards x2
+                </Text>
+            </View>
+            <View>
+                <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
+                    Próximos eventos del mes:
+                </Text>
+                <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
+                    PetEvents x3
+                </Text>
+            </View>
+        </View>
     );
 };
