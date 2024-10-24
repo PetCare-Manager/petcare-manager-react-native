4f7e14ba012386590e09d5329c8a75f2acbedb2e
Carol-88 <gralleta@gmail.com>
2024-10-24 10:49:17 +0200

    🚧  work in ui

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -13,31 +13,37 @@ type HomeScreenProps = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Image source={logo} className="!w-[160px] !h-[220px] mb-8" />
-      <View className="w-full px-6">
-        <Text className="font-afacad-semibold text-3xl text-center text-gray-800 mb-8">
-          PetCare Manager
-        </Text>
+    <View className="flex-1 bg-white justify-between items-center">
+      <Image source={logo} className="w-68 h-52 mt-28" />
+      <View className="items-center">
+        <View className="w-full px-10">
+          <Text className="font-afacad-semibold text-4xl text-center text-slate-600 mb-8">
+            PetCare Manager
+          </Text>
 
-        <Text className="font-raleway-regular text-l text-center text-gray-800 mb-28">
-          Lleva todo lo que necesitas en el bolsillo
-        </Text>
-
-        {/* Contenedor para los botones en fila */}
-        <View className="flex-row justify-between mb-4">
+          <Text className="font-raleway-light text-xl text-center text-gray-800">
+            Todo lo que necesitas de tu mascota, en tu bolsillo, a un solo
+            click.
+          </Text>
+        </View>
+      </View>
+      {/* Contenedor para los botones en la parte inferior */}
+      <View className="w-full">
+        <View className="flex-row justify-between mb-10 mx-12">
           <TouchableOpacity
             onPress={() => navigation.navigate("Register")}
-            className="bg-white rounded-lg border border-indigo-500 py-2 px-4 flex-1 mr-2 items-center justify-center"
+            className="bg-white rounded-lg w-32 h-12 border border-indigo-500 py-2 px-4 items-center justify-center"
           >
-            <Text className="text-indigo-500">Registro</Text>
+            <Text className="text-indigo-500 font-raleway-regular">
+              Registro
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
             onPress={() => navigation.navigate("Login")}
-            className="bg-indigo-500 rounded-lg py-2 px-4 flex-1 ml-2 items-center justify-center"
+            className="bg-indigo-500 w-32 h-12 rounded-lg py-2 px-4 items-center justify-center"
           >
-            <Text className="text-white">Login</Text>
+            <Text className="text-white font-raleway-regular">Login</Text>
           </TouchableOpacity>
         </View>
       </View>
